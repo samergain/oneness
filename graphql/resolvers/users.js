839ad3b834f4e5116707c41e17fb3a2d41d89d12
samergain <samersadon@gmail.com>
2021-02-19 19:55:39 -0500

    added validation for signup and login

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,17 +1,62 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { UserInputError } = require('apollo-server');
+
+const { validateRegisterInput, validateLoginInput } = require('../../utils/validators')
 const { SECRET_KEY } = require('../../config');
 const User = require('../../models/User');
 
+function generateToken(user) {
+    return jwt.sign(
+        {
+            id: user.id,
+            email: user.email,
+            username: user.username
+        }, 
+        SECRET_KEY,
+        { expiresIn: '1h' }
+        );
+}
 module.exports = {
     Mutation: {
+        async login(_, { username, password }){
+            const {errors, valid} = validateLoginInput(username, password);
+
+            if(!valid){
+                throw new UserInputError('Errors', { errors });
+            }
+            const user = await User.findOne({ username });
+
+            if(!user){
+                errors.general = 'User not found';
+                throw new UserInputError('User not found', { errors });
+            }
+
+            const match = await bcrypt.compare(password, user.password);
+            if(!match){
+                errors.general = 'Wrong credentials';
+                throw new UserInputError('Wrong credentials', { errors });
+            }
+
+            const token = generateToken(user);
+            return {
+                ...user._doc,
+                id: user._id,
+                token
+            };
+
+            
+        },
         async register(
             _, 
             {
                 registerInput: { username, email, password, confirmPassword }
             }){
-            // TODO: Validate user data
+            // Validate user data
+            const { valid, errors } = validateRegisterInput(username, email, password, confirmPassword);
+            if(!valid) {
+                throw new UserInputError('Errors', { errors });
+            }
             // TODO: Make sure user doesn't already exist
             const user = await User.findOne({ username });
             if(user) {
@@ -31,15 +76,7 @@ module.exports = {
             });
 
             const res = await newUser.save();
-            const token = jwt.sign(
-            {
-                id: res.id,
-                email: res.email,
-                username: res.username
-            }, 
-            SECRET_KEY,
-            { expiresIn: '1h' }
-            );
+            const token = generateToken(res)
             return {
                 ...res._doc,
                 id: res._id,
