3a96f95f65b9848e1f00e4da4deb78f845c36a85
samergain <samersadon@gmail.com>
2021-02-23 19:57:03 -0500

    deletePost is working fine

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,3 +1,5 @@
+const { AuthenticationError } = require('apollo-server');
+
 const Post = require('../../models/Post');
 const checkAuth = require('../../utils/check-auth');
 
@@ -37,6 +39,21 @@ module.exports = {
 
                 const post = await newPost.save()
                 return post;
+            },
+            async deletePost(_, { postId }, context){
+                const user = checkAuth(context);
+
+                try{
+                    const post = await Post.findById(postId);
+                    if(user.username === post.username){
+                        await post.delete();
+                        return 'Post deleted successfully';
+                    } else {
+                        throw new AuthenticationError('Action not allowed');
+                    }
+                } catch(err) {
+                    throw new Error(err);
+                }
             }
         }
 };
